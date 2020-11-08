6c16f478c458afc2fe6a48bfa620b1ab076432bd
admin <850943653@qq.com>
2020-11-08 09:38:44 +0800

    2020-11-08

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"zx/rout"
+)
+
+func main() {
+	r := gin.Default()
+	rout.Rout(r)
+	r.Run(":9000")
+}
---
+++ migrate/migrate.go
@@ -0,0 +1,12 @@
+package migrate
+
+import (
+	"zx/database"
+	"zx/model"
+)
+
+var db = database.Init()
+
+func Model() {
+	db.AutoMigrate(&model.User{})
+}
